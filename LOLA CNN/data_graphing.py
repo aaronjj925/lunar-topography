fc2cb70e47db751afe50c23fa739a2c65c8fe87b
Aaron Jung <jjung999@terpmail.umd.edu>
2026-04-13 21:15:40 -0400

    Update data_graphing.py

--- LOLA CNN/data_graphing.py
+++ LOLA CNN/data_graphing.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import torch
 
+##### must change file paths for different laptops #####
+
 def read_graph(filePath, lonmin, lonmax, latmin, latmax):
     # read csv file
     df = pd.read_csv(filePath)
@@ -19,20 +21,20 @@ def read_graph(filePath, lonmin, lonmax, latmin, latmax):
     lat = (torch.tensor(lat.values, dtype=torch.float32))
     rad = (torch.tensor(rad.values, dtype=torch.float32))
 
-    # # scatter plot
-    # plt.figure(figsize=(8,6))
-    # scat = plt.scatter(lon, lat, c=rad, s=10)
-    # plt.colorbar(scat, label='Radius')
-    # # scat = plt.scatter(lat, rad, c=lon, s=5)
-    # # plt.colorbar(scat, label="longitude")
+    # scatter plot
+    plt.figure(figsize=(8,6))
+    scat = plt.scatter(lon, lat, c=rad, s=10)
+    plt.colorbar(scat, label='Radius')
+    # scat = plt.scatter(lat, rad, c=lon, s=5)
+    # plt.colorbar(scat, label="longitude")
 
-    # # # label setting
-    # plt.xlabel('Longitude')
-    # plt.ylabel('Latitude')
-    # plt.title('Heatmap of Altitude by Lat/Lon')
-    # print(len(lon))
+    # label setting
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.title('Heatmap of Altitude by Lat/Lon')
+    print(len(lon))
     return df
 
-# read_graph("C:/Users/16679/Desktop/LOLA_DataFolder/filterMain.csv", 14, 16, -17, -15)
-# plt.show()
+read_graph("C:/Users/16679/Desktop/LOLA_DataFolder/filterMain.csv", 14, 16, -17, -15)
+plt.show()
 # filterMain goes for around lon=(13.5,19.75), lat=(-14.75,-18.5)
